9f47e55a4dd36da2164854d5bcf4e0d8bbd973ef
megha-pathak <meghapathak2013@gmail.com>
2022-06-10 09:49:39 +0530

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage/LandingPage";
-import Login from "./pages/Login/Login";
-import Signup from "./pages/Signup/Signup";
-import TaskPage from "./pages/TaskPage/TaskPage";
-import Task from "./pages/Task/Task";
 
 function App() {
   return <div className="App"> hello world </div>;
