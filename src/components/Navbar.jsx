dcbc8974015ac165163a8b9975df829f6fa861a2
megha-pathak <meghapathak2013@gmail.com>
2022-06-10 14:25:16 +0530

    Add Home Page and NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuthContext } from "../context/auth-context";
+import { useAuth } from "../context/auth-context";
 
 const Navbar = () => {
-  const { auth, setAuth } = useAuthContext();
+  const { auth, setAuth } = useAuth();
 
   const navigate = useNavigate();
 
@@ -18,57 +18,46 @@ const Navbar = () => {
       token: localStorage.getItem("USER_TOKEN"),
       user: JSON.parse(localStorage.getItem("USER_DATA")),
     });
+
     navigate("/login");
   };
 
   return (
-    <nav class="bg-white border-gray-200 px-2 py-2">
-      <div class="container mx-auto flex flex-wrap items-center justify-between">
-        <div class="flex">
-          <Link to="/">
-            <span class="self-center text-lg font-semibold whitespace-nowrap">
-              Potato Clock
-            </span>
+    <nav className="Navbar border-0 border-teal-400 flex flex-row justify-between p-2">
+      <div className="left-navbar flex flex-row items-center ml-12">
+        <div className="brand-logo"></div>
+        <Link to="/">
+          <div className="brand-name text-teal-100 font-bold text-3xl">
+            Potato Clock
+          </div>
+        </Link>
+      </div>
+      <div className="right-navbar flex flex-row items-center justify-between mr-12">
+        <button
+          onClick={() => {
+            auth.isLoggedIn ? navigate("/tasks") : navigate("/login");
+          }}
+          className="font-bold text-lg ml-8 text-teal-100"
+        >
+          tasks
+        </button>
+
+        {auth.isLoggedIn ? (
+          <div className="ml-8 mr-8 flex flex-col items-start text-teal-100 relative">
+            <button
+              onClick={logoutHandler}
+              className="bg-cyan-400 text-white font-medium px-6 py-2 rounded-2xl rounded-bl-none ml-8 mr-8"
+            >
+              Logout
+            </button>
+          </div>
+        ) : (
+          <Link to="/login">
+            <button className="bg-cyan-400 text-white font-medium px-6 py-2 rounded-2xl rounded-bl-none ml-8 mr-8">
+              Login
+            </button>
           </Link>
-        </div>
-
-        <div class=" md:block w-full md:w-auto" id="mobile-menu">
-          <ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
-            <li>
-              <div
-                class="bg-blue-700 md:bg-transparent text-white block pl-3 pr-4 py-2 md:text-blue-700 md:p-0 rounded"
-                aria-current="page"
-              >
-                <Link to="/">Home</Link>
-              </div>
-            </li>
-            <li>
-              <div class="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">
-                <button
-                  onClick={() => {
-                    auth.isLoggedIn ? navigate("/tasks") : navigate("/login");
-                  }}
-                >
-                  Tasks
-                </button>
-              </div>
-            </li>
-
-            {auth.isLoggedIn ? (
-              <li>
-                <div class="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">
-                  <button onClick={logoutHandler}>Logout</button>
-                </div>
-              </li>
-            ) : (
-              <li>
-                <div class="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">
-                  <Link to="/login">Login</Link>
-                </div>
-              </li>
-            )}
-          </ul>
-        </div>
+        )}
       </div>
     </nav>
   );
